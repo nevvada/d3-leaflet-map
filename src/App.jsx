5ff271bcd3d44610337c3827d9d12e6c6d486dba
nlemast <nlemaster47@gmail.com>
2019-09-10 19:18:12 -0400

    configured App component to receive lat/lng from search and pass to map

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import Map from './components/Map';
 
 import '../scss/styles.scss';
-import SearchBar from './components/SearchBar';
+// import SearchBar from './components/SearchBar';
+import LocationSearchInput from './components/LocationSearchInput';
 
 export class App extends Component {
+  state = {
+    clickedLatLng: { lat: 0, lng: 0 }
+  };
+
+  retrieveLatLng = coordinates => {
+    this.setState({
+      clickedLatLng: {
+        ...this.state.clickedLatLng,
+        lat: coordinates.lat,
+        lng: coordinates.lng
+      }
+    });
+  };
+
   render() {
     return (
       <>
-        <SearchBar />
-        <Map />
+        {/* <SearchBar /> */}
+        <LocationSearchInput retrieveLatLng={this.retrieveLatLng} />
+        <Map clickedLatLng={this.state.clickedLatLng} />
       </>
     );
   }
